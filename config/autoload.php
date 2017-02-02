e4a37c6a7dd3728955e0c93034f103b21b1b87df
Valentin <exatre@gmail.com>
2017-02-02 11:12:00 +0100

    Update autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -1,15 +1,6 @@
 <?php
 
 /**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2015 Leo Feyer
- *
- * @license LGPL-3.0+
- */
-
-
-/**
  * Register the namespaces
  */
 ClassLoader::addNamespaces(array
