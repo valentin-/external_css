9487e0be71ec3e12297a689f488fdf7dc88fbd8f
Valentin <exatre@gmail.com>
2017-02-17 08:50:11 +0100

    Update tl_user.php

--- languages/de/tl_user.php
+++ languages/de/tl_user.php
@@ -1,4 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_user']['external_css_legend'] = 'External CSS';
-$GLOBALS['TL_LANG']['tl_user']['external_css_livereload'] = array('Livereload verwenden', '');
+$GLOBALS['TL_LANG']['tl_user']['external_css_livereload'] = array('Livereload verwenden', 'Map files in Chrome Dev Tools');
